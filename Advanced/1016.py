d29178988150608e041a42fc9a97f3cd10ebc169
absop <2913049342@qq.com>
2019-07-12 23:34:45 +0800

    Add Python solution for 1016, simple but slow.

---
+++ Advanced/1016.py
@@ -0,0 +1,49 @@
+
+def tominute(time):
+    day, hour, minute = time
+    return (day * 24 + hour) * 60 + minute
+
+def bill(time):
+    day, hour, minute = time
+    charge = day * rate[24]
+    charge += sum(rate[:hour])
+    charge += rate[hour] * minute / 60.0
+    return charge
+
+customer, paired = {}, []
+rate = [0.6 * int(i) for i in input().split(" ")]
+rate.append(sum(rate))
+
+for i in range(int(input())):
+    name, time, status = input().split(" ")
+
+    if name not in customer:
+        customer[name] = []
+
+    customer[name].append((time, status == "on-line"))
+
+for name in sorted(customer):
+    record = customer[name]
+    record.sort(key=lambda x: x[0])
+    i, pair = 1, []
+    while i < len(record):
+        if(record[i - 1][1] and not record[i][1]):
+            pair.append(i)
+            i += 1
+        i += 1
+    if pair:
+        paired.append((name, pair))
+
+for name, pair in paired:
+    total = 0
+    record = customer[name]
+    print(name, record[0][0][:2])
+    for i in pair:
+        t1, t2 = record[i - 1][0][3:], record[i][0][3:]
+        time1 = [int(i) for i in t1.split(":")]
+        time2 = [int(i) for i in t2.split(":")]
+        minute = tominute(time2) - tominute(time1)
+        once =  bill(time2) - bill(time1)
+        total += once
+        print(t1, t2, minute, "$%.2f"%once)
+    print("Total amount: $%.2f"%total)

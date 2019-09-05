165568284805f0d6e6d706ef5fbef529b5142183
absop <2913049342@qq.com>
2019-09-06 00:18:03 +0800

    Add solutions for 1035, 1036, state for 1037.

--- Advanced/1035.c
+++ Advanced/1035.c
@@ -1,87 +1,35 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
 #include <string.h>
-
-#define val(c) (c - 'A')
-#define chr(v) (v + 'A')
-#define hash(s) ((val(s[0]) * 26 + val(s[1])) * 26 + val(s[2]))
-
-int idcnt = 1, anscnt;
-int number[17576], string[2010];
-int G[2010][2010], W[2010];
-bool visit[2010];
-struct _gang {
-    int head;
-    int num;
-} ans[2010];
-
-void tostring(int hash, char *name)
-{
-    name[2] = chr(hash % 26); hash /= 26;
-    name[1] = chr(hash % 26); hash /= 26;
-    name[0] = chr(hash % 26);
-}
-
-int tonumber(char *name)
-{
-    int hash = hash(name);
-    if (number[hash] == 0) {
-        number[hash] = idcnt;
-        string[idcnt++] = hash;
-    }
-    return number[hash];
-}
-
-#define cvptr const void *
-#define head(ptr) ((struct _gang*)ptr)->head
-int cmp(cvptr a, cvptr b) { return head(a) < head(b) ? -1 : 1; }
-
-void dfs(int u, int *head, int *numMember, int *totalweight)
-{
-    visit[u] = true;
-    ++*numMember;
-    if (W[u] > W[*head])
-        *head = u;
-    for (int v = 1; v < idcnt; v++) {
-        if (G[u][v] > 0) {
-            *totalweight += G[u][v];
-            G[u][v] = G[v][u] = 0;
-            if (!visit[v])
-                dfs(v, head, numMember, totalweight);
-        }
-    }
-}
-
 
 int main()
 {
-    int N, K, time, id1, id2;
-    char s1[4], s2[4];
-    scanf("%d %d", &N, &K);
+    int changed, N, cnt = 0;
+    char modified[1000][24], buffer[16] = {32};
+    char *password = buffer + 1, *p;
+    const char *fmts[2] = {
+        "There is %d account %s",
+        "There are %d accounts %s"
+    };
+
+    scanf("%d", &N);
     for (int i = 0; i < N; ++i) {
-        scanf("%s %s %d", s1, s2, &time);
-        id1 = tonumber(s1);
-        id2 = tonumber(s2);
-        W[id1] += time;
-        W[id2] += time;
-        G[id1][id2] += time;
-        G[id2][id1] += time;
+        scanf("%s %s", modified[cnt], password);
+        for (changed = 0, p = password; *p; ++p) {
+            // 1 -> @, 0 -> %, l -> L, O -> o
+            if      (*p == '1') { changed = 1; *p = '@'; }
+            else if (*p == '0') { changed = 1; *p = '%'; }
+            else if (*p == 'l') { changed = 1; *p = 'L'; }
+            else if (*p == 'O') { changed = 1; *p = 'o'; }
+        }
+        if (changed) strcat(modified[cnt++], buffer);
     }
 
-    for (int u = 1; u < idcnt; ++u) {
-        if (!visit[u]) {
-            int head = u, numMember = 0, totalweight = 0;
-            dfs(u, &head, &numMember, &totalweight);
-            if (numMember > 2 && totalweight > K) {
-                ans[anscnt++] = (struct _gang) {string[head], numMember};
-            }
+    if (cnt == 0)
+        printf(fmts[N > 1], N, "and no account is modified\n");
+    else {
+        printf("%d\n", cnt);
+        for (int i = 0; i < cnt; ++i) {
+            puts(modified[i]);
         }
-    }
-    printf("%d\n", anscnt);
-    qsort(ans, anscnt, sizeof(ans[0]), cmp);
-    for (int i = 0; i < anscnt; ++i) {
-        tostring(ans[i].head, s1);
-        printf("%s %d\n", s1, ans[i].num);
     }
 }
---
+++ Advanced/1036.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct _student_t student_t;
+struct _student_t
+{
+    char name[12];
+    char id[12];
+    int grade;
+};
+
+int Nmale, Nfemale;
+int femaleHighest = -1, maleLowest = 101;
+student_t *ptrfemale, *ptrmale;
+student_t maleStudents[101], femaleStudents[101];
+
+int main()
+{
+    int N, grade;
+    char gender, name[12], id[12];
+
+    scanf("%d", &N);
+    for (int i = 0; i < N; ++i) {
+        scanf("%s %c %s %d", name, &gender, id, &grade);
+        if (gender == 'M') {
+            strcpy(maleStudents[Nmale].name, name);
+            strcpy(maleStudents[Nmale].id, id);
+            maleStudents[Nmale++].grade = grade;
+        }
+        else /*if (gender == 'F')*/ {
+            strcpy(femaleStudents[Nfemale].name, name);
+            strcpy(femaleStudents[Nfemale].id, id);
+            femaleStudents[Nfemale++].grade = grade;
+        }
+    }
+
+    for (int i = 0; i < Nfemale; ++i) {
+        if (femaleStudents[i].grade > femaleHighest) {
+            ptrfemale = &femaleStudents[i];
+            femaleHighest = ptrfemale->grade;
+        }
+    }
+    for (int i = 0; i < Nmale; ++i) {
+        if (maleStudents[i].grade < maleLowest) {
+            ptrmale = &maleStudents[i];
+            maleLowest = ptrmale->grade;
+        }
+    }
+
+    if (femaleHighest != -1)
+        printf("%s %s\n", ptrfemale->name, ptrfemale->id);
+    else printf("Absent\n");
+
+    if (maleLowest != 101)
+        printf("%s %s\n", ptrmale->name, ptrmale->id);
+    else printf("Absent\n");
+
+    if (femaleHighest == -1 || maleLowest == 101)
+        printf("NA\n");
+    else printf("%d\n", femaleHighest - maleLowest);
+}

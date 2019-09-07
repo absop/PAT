10826b8146cf0b8108085eabcdc23cfc4d1ea4ec
叶芝秋 <32818900+absop@users.noreply.github.com>
2019-09-08 05:07:05 +0800

    Update 1010.c

--- Advanced/1010.c
+++ Advanced/1010.c
@@ -2,15 +2,14 @@
 
 int main()
 {
-    int tag, radix;
     char N1[16], N2[16], *p, *pk = N1, *pu = N2;
     long long kv, uv, minr, midr, maxr, vchr[128] = {0};
     for (int i = '0'; i <= '9'; ++i) vchr[i] = i - '0';
     for (int i = 'a'; i <= 'z'; ++i) vchr[i] = i - 'a' + 10;
     
-    scanf("%s %s %d %d", N1, N2, &tag, &radix);
-    if (tag == 2) pk = N2, pu = N1;
-    for (p = pk, kv = vchr[*p]; *++p; kv = kv * radix + vchr[*p]);
+    scanf("%s %s %lld %lld", N1, N2, &kv, &uv);
+    if (kv == 2) pk = N2, pu = N1;
+    for (p = pk, kv = vchr[*p]; *++p; kv = kv * uv + vchr[*p]);
     for (p = pu, minr = *p; *++p; *p > minr && (minr = *p));
     for (minr = vchr[minr] + 1, maxr = kv + 1; minr <= maxr;) {
         midr = (minr + maxr) >> 1;

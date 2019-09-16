0c81ccee3e08134de78592ea5048f8a9634850cc
absop <2913049342@qq.com>
2019-09-16 20:00:24 +0800

    Redo 1029, better algorithm and simpler code.

--- AdvancedLevel_C/1029.c
+++ AdvancedLevel_C/1029.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
+#include <limits.h>
 
-int k[200001];
+inline int getint()
+{
+    int val = 0, c = getchar();
+    for (; c < '0' || c > '9'; c = getchar());
+    for (; '0' <= c && c <= '9'; c = getchar())
+        val = val * 10 + c - '0';
+    return val;
+}
 
-#define TEST_RETURN(x)       \
-    if (++count == midpos) { \
-        printf("%d", x);     \
-        return 0;            \
-    }
+int A[200001];
 
 int main()
 {
-    int n, m, temp, count = 0;
-    scanf("%d", &n);
-    for (int i = 0; i < n; ++i) scanf("%d", &k[i]);
-    k[n] = 0x7fffffff;
-    scanf("%d", &m);
-    int midpos = (n + m + 1) / 2, i = 0;
-    for (int j = 0; j < m; ++j) {
-        scanf("%d", &temp);
-        while (k[i] < temp) {
-            TEST_RETURN(k[i])
-            ++i;
+    int m, n, mid, x;
+
+    m = getint();
+    for (int i = 0; i < m; i++)
+        A[i] = getint();
+    A[m] = INT_MAX;
+
+    n = getint(), x = getint();
+    mid = (m + n + 1) / 2;
+    int median, j = 0, rq = 1;
+    for (int i = 0; i < mid; i++) {
+        if (A[j] <= x)
+            median = A[j++];
+        else {
+            median = x;
+            x = ++rq <= n ? getint() : INT_MAX;
         }
-        TEST_RETURN(temp)
     }
-    while (i < n) {
-        TEST_RETURN(k[i])
-        ++i;
-    }
+
+    printf("%d", median);
+
     return 0;
 }
